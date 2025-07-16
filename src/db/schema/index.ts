99abd2a97fdcf7880f0aefb9256ec4d13884505f
UlyssesFerreira <ulyssesh.20@gmail.com>
2025-07-16 16:27:53 -0300

    create room, create questions e get room questions

--- src/db/schema/index.ts
+++ src/db/schema/index.ts
@@ -1,6 +1,8 @@
 // Barrel file (arquivo para exportar todos os outros arquivos da pasta)
+import { questions } from './questions.ts'
 import { rooms } from './rooms.ts'
 
 export const schema = {
   rooms,
+  questions,
 }
