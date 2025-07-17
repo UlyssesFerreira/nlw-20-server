ff3ccc2686da2a6ec8d9effab1e1927df5f97dec
UlyssesFerreira <ulyssesh.20@gmail.com>
2025-07-17 11:49:48 -0300

    seeds

--- src/db/seed.ts
+++ src/db/seed.ts
@@ -7,7 +7,7 @@ await reset(db, schema) // Zerar db antes do seed
 await seed(db, schema).refine((f) => {
   return {
     rooms: {
-      count: 20,
+      count: 5,
       columns: {
         name: f.companyName(),
         description: f.loremIpsum(),
